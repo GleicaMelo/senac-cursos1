017f134504b4f8888a4fe9a6a2c0688e8e86ef4f
GleicaMelo <gleicamelo36@gmail.com>
2025-10-30 17:15:45 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
-    <nav className="bg-white py-10 px-20 text-blue-600 p-4">
-      <h1 className="text-3xl font-bold"> Senac Cursos</h1>
-      
+    <nav className="bg-white py-10 px-20 text-blue-600 p-4 flex items-center justify-between">
+      <h1 className="text-3xl font-bold">Senac Cursos</h1>
+      <img src="/src/assets/images.png" alt="senac" className="h-9" />
     </nav>
   );
 }
