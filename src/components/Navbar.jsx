8a5884b2ed1e93c72a7b8ad8f35f12bb02d26925
felphs3d2y <filypegalvao17@gmail.com>
2025-10-31 15:37:02 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
-
+// aqui é onde a gente pode mexer no Navbar
 export default function Navbar() {
   return (
-    <nav className="bg-white py-10 px-20 text-blue-600 p-4">
-      <h1 className="text-3xl font-bold"> Senac Cursos</h1>
-      
+    <nav className="bg-white py-5 px-10 text-blue-600 p-4 flex items-center justify-between">
+        <img src="/src/assets/images.png" alt="senac" className="h-12" />
+
+      <div className="flex space-x-4">
+
+        <Link to="/login">
+          <button className="text-blue-600 px-4 py-2 rounded hover:text-orange-500">
+            Login
+          </button>
+        </Link>
+
+        
+
+      </div>
     </nav>
   );
 }
