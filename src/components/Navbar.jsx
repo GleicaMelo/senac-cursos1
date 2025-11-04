1076f67fbd3157207d31538989a1e4ccbf6d2be0
felphs3d2y <filypegalvao17@gmail.com>
2025-11-04 14:59:49 -0300

    Refactor Navbar component and update links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
-// aqui é onde a gente pode mexer no Navbar
+
+// Aqui é onde a gente pode mexer no Navbar
 export default function Navbar() {
   return (
-    <nav className="bg-gradient-to-t from-blue-100 to-orange-100 py-5 px-10 text-blue-600 p-4 flex items-center justify-between">
-        <img src="/src/assets/senac-logo.png" alt="senac" className="h-18" />
+    <nav className="bg-gradient-to-t from-blue-100 to-orange-100 py-5 px-10 text-blue-600 flex items-center justify-between shadow-sm">
+      {/* Logo clicável */}
+      <Link to="/">
+        <img
+          src="/src/assets/senac-logo.png"
+          alt="Senac"
+          className="h-18 hover:opacity-80 transition duration-200"
+        />
+      </Link>
 
       <div className="flex space-x-4">
-
-        <Link to="/login">
-          <button className="text-blue-600 px-4 py-2 rounded hover:text-orange-500">
+        <Link to="/cursos.jsx">
+          <button className="text-blue-600 px-4 py-2 rounded hover:text-orange-500 transition duration-200">
             Login
           </button>
         </Link>
-
-        
-
       </div>
     </nav>
   );
