7b867d971c1b6b16b2a3b7a2c7f21562b409c2df
felphs3d2y <filypegalvao17@gmail.com>
2025-11-04 17:50:12 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
+{ Link } from "react-router-dom";
+import { LogIn } from "lucide-react"; 
 
-// Aqui é onde a gente pode mexer no Navbar
 export default function Navbar() {
   return (
     <nav className="bg-gradient-to-t from-blue-100 to-orange-100 py-5 px-10 text-blue-600 flex items-center justify-between shadow-sm">
@@ -9,17 +9,20 @@ export default function Navbar() {
         <img
           src="/src/assets/senac-logo.png"
           alt="Senac"
-          className="h-18 hover:opacity-80 transition duration-200"
+          className="h-16 hover:opacity-80 transition duration-200"
         />
       </Link>
 
-      <div className="flex space-x-4">
-        <Link to="/cursos.jsx">
-          <button className="text-blue-600 px-4 py-2 rounded hover:text-orange-500 transition duration-200">
-            Login
-          </button>
-        </Link>
-      </div>
+      {/* Botão de Login */}
+      <Link to="/login">
+        <button className="relative flex items-center px-6 py-2 rounded-full bg-white text-blue-300 hover:text-white font-semibold shadow-md hover:bg-blue-300 transition-all duration-300 border-4 border-blue-300 hover:border-white">
+          {/* Círculo com ícone */}
+          <span className="hover:bg-blue-300 absolute left-0 flex items-center justify-center w-10 h-10 rounded-full bg-white border-3 border-blue-300 hover:border-white -translate-x-3 shadow-lg">
+            <LogIn className="w-5 h-5 text-blue-300 hover:bg-blue-300 hover:text-white  " />
+          </span>
+          <span className="ml-6 ">LOGIN</span>
+        </button>
+      </Link>
     </nav>
   );
 }
