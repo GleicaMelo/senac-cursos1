4bdd817fa0b5e28107962fb51781d8acd30c4142
felphs3d2y <filypegalvao17@gmail.com>
2025-11-07 15:10:58 -0300

    Adjust Navbar styles and import statement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-{ Link } from "react-router-dom";
+
+
+import { Link } from "react-router-dom";
 import { LogIn } from "lucide-react"; 
 
 export default function Navbar() {
@@ -9,13 +11,13 @@ export default function Navbar() {
         <img
           src="/src/assets/senac-logo.png"
           alt="Senac"
-          className="h-16 hover:opacity-80 transition duration-200"
+          className="h-20 hover:opacity-80 transition duration-200"
         />
       </Link>
 
       {/* Botão de Login */}
       <Link to="/login">
-        <button className="relative flex items-center px-6 py-2 rounded-full bg-white text-blue-300 hover:text-white font-semibold shadow-md hover:bg-blue-300 transition-all duration-300 border-4 border-blue-300 hover:border-white">
+        <button className="relative flex items-center px-4 py-1 rounded-full bg-white text-blue-300 hover:text-white font-semibold shadow-md hover:bg-blue-300 transition-all duration-300 border-4 border-blue-300 hover:border-white">
           {/* Círculo com ícone */}
           <span className="hover:bg-blue-300 absolute left-0 flex items-center justify-center w-10 h-10 rounded-full bg-white border-3 border-blue-300 hover:border-white -translate-x-3 shadow-lg">
             <LogIn className="w-5 h-5 text-blue-300 hover:bg-blue-300 hover:text-white  " />
