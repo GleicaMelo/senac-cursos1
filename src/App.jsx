3e23e6c3f26fc27b68186e9818b1799852ca4cd6
felphs3d2y <filypegalvao17@gmail.com>
2025-11-10 16:30:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cursos from "./pages/cursos";
 import Cadastro from "./pages/cadastro";
 import Navbar from "./components/Navbar";
+import QuemSomos from "./pages/QuemSomos";
+import OndeEstamos from "./pages/OndeEstamos";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Cursos />} />
         <Route path="/cadastro/:id" element={<Cadastro />} />
+        <Route path="/QuemSomos" element={<QuemSomos />} />
+        <Route path="/OndeEstamos" element={<OndeEstamos />} />
       </Routes>
     </Router>
   );
