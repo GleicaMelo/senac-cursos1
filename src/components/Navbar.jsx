1ec51b9d783b1772efd6d280daa24f9a3a65c33f
filype <filypegalvao17@gmail.com>
2025-12-22 16:38:34 -0300

    alteração navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,24 @@ import { LogIn, ShieldCheck } from "lucide-react";
 
 export default function Navbar() {
   const location = useLocation();
-  const isAdminPage = location.pathname.startsWith("/admin");
+
+  const isAdminPage =
+    location.pathname === "/admin" ||
+    location.pathname === "/admin-login";
 
   return (
-    <nav className="bg-gradient-to-t from-blue-100 to-orange-100 py-5 px-10 text-blue-600 flex items-center justify-between shadow-sm">
+    <nav className="bg-gradient-to-t from-blue-100 to-orange-100 py-5 px-10 flex items-center justify-between shadow-sm">
       
-      {/* Logo SEMPRE */}
+      {/* LOGO — SEMPRE VISÍVEL */}
       <Link to="/">
         <img
-          src="/src/assets/senac-logo.png"
+          src="/senac-logo.png"
           alt="Senac"
           className="h-20 hover:opacity-80 transition duration-200"
         />
       </Link>
 
-      {/* AÇÕES — só se NÃO for admin */}
+      {/* AÇÕES — SOMEM NO ADMIN */}
       {!isAdminPage && (
         <div className="flex items-center gap-4">
 
@@ -31,11 +34,9 @@ export default function Navbar() {
 
           {/* LOGIN */}
           <Link to="/login">
-            <button className="relative flex items-center px-4 py-1 rounded-full bg-white text-blue-300 hover:text-white font-semibold shadow-md hover:bg-blue-300 transition-all duration-300 border-4 border-blue-300 hover:border-white">
-              <span className="absolute left-0 flex items-center justify-center w-10 h-10 rounded-full bg-white border-3 border-blue-300 -translate-x-3 shadow-lg">
-                <LogIn className="w-5 h-5 text-blue-300" />
-              </span>
-              <span className="ml-6">LOGIN</span>
+            <button className="flex items-center gap-2 px-4 py-2 rounded-full bg-white text-blue-400 font-semibold shadow-md hover:bg-blue-300 hover:text-white transition-all duration-300 border-4 border-blue-300">
+              <LogIn className="w-5 h-5" />
+              LOGIN
             </button>
           </Link>
 
